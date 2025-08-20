e6595cd5a9b2a31c4198373829f601f534068232
GuilhermeSilvaCarvalho11 <137510498+GuilhermeSilvaCarvalho11@users.noreply.github.com>
2025-08-20 20:27:45 -0300

    s

--- script.js
+++ script.js
@@ -3,58 +3,100 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-
 const perguntas = [
 {
-enunciado: "A energia solar é uma fonte de energia renovável e sustentável que é obtida
-apartir da luz do sol. Diante disso: ",
+enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que
+consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios
+hiper-realistas. Qual o primeiro pensamento?",
 alternativas: [
 {
-texto: "Você investiria em energia solar?",
-afirmacao: "Você é uma pessoa preocupada com o meio ambiente e pensa na
-
-economia ao longo prazo, bem como energia limpa e renovável."
+texto: "Isso é assustador!",
+afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
 },
 {
-texto: "Você continua com a energia hidreelétrica?",
-afirmacao: "Você é resistente a mudanças e prefere a praticidade da energia que
-
-conhece "
+texto: "Isso é maravilhoso!",
+afirmacao: "Quis saber como usar IA no seu dia a dia."
 }
 ]
 },
 {
-enunciado: "Pensando em energia renovável e sustentável, e em recursos naturais
-inesgotáveis ",
+enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma
+professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No
+fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual
+atitude você toma?",
 alternativas: [
 {
-texto: "Você pensa na energia solar como renovável e sustentável",
-afirmacao: "Esta fonte aproveita a radiação solar ou o calor para gerar eletricidade, o
-que a torna uma alternativa limpa e sustentável às fontes de energia convencionais que emitem
-poluentes atmosféricos",
+
+texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a
+encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o
+entendimento.",
+afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
 },
 {
-texto: "Acredita que a energia hidreelétrica é uma energia renovável e sustentável",
-afirmacao: " é uma energia renovável - ilimitada no tempo - porque são as suas
-fontes que se renovam, ou seja, rios e riachos alimentados pelo ciclo natural da água. "
+texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas
+pesquisas na internet e conhecimentos próprios sobre o tema.",
+afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu
+trabalho."
 }
 ]
 },
 {
-enunciado: "Refletindo em vantagens entre a energia solar e hidreelétrica ",
+enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a
+turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado
+um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se
+posiciona?",
 alternativas: [
 {
-texto: "Acredita nos pontos positivos da energia solar",
-
-afirmacao: "A energia solar não polui, é renovável, limpa e silenciosa, pode ser usada
-em áreas isoladas da rede elétrica, necessidade mínima de manutenção, muito fácil de instalar
-e é barato para manter"
+texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e
+melhorar habilidades humanas.",
+afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos
+caminhos profissionais com IA."
 },
 {
-texto: "Defende a ideia nas vantagens da energia hidreelétrica",
-afirmacao: "As principais vantagens para promover a energia hidrelétrica é a alta
-produção de energia dos seus projetos, por meio das barragens, e na geração elétrica
-sustentável por meio de uma fonte renovável, que é a água. "
+texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e
+defendem a importância de proteger os trabalhadores.",
+afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre
+trabalhadores para discutir meios de utilização de IA de forma ética."
+}
+]
+},
+{
+enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que
+representasse o que pensa sobre IA. E agora?",
+alternativas: [
+{
+texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas
+tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura
+digital para iniciantes."
+},
+{
+texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem
+e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar
+também!"
+}
+]
+},
+{
+enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o
+andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda
+da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+alternativas: [
+{
+
+texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por
+isso não é um problema utilizar o texto inteiro.",
+afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se
+sente dependente da IA para tudo."
+},
+{
+texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção
+pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é
+essencial.",
+afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que
+programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos
+gerados pela IA devem servir como auxílio e não resultado final. "
 }
 ]
 },
@@ -63,7 +105,6 @@ sustentável por meio de uma fonte renovável, que é a água. "
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
-
 function mostraPergunta() {
 if (atual >= perguntas.length) {
 mostraResultado();
@@ -74,7 +115,6 @@ caixaPerguntas.textContent = perguntaAtual.enunciado;
 caixaAlternativas.textContent = "";
 mostraAlternativas();
 }
-
 function mostraAlternativas(){
 for(const alternativa of perguntaAtual.alternativas) {
 const botaoAlternativas = document.createElement("button");
@@ -83,18 +123,15 @@ botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativ
 caixaAlternativas.appendChild(botaoAlternativas);
 }
 }
-
 function respostaSelecionada(opcaoSelecionada) {
 const afirmacoes = opcaoSelecionada.afirmacao;
 historiaFinal += afirmacoes + " ";
 atual++;
 mostraPergunta();
 }
-
 function mostraResultado() {
-caixaPerguntas.textContent = "Em 2024...";
+caixaPerguntas.textContent = "Em 2049...";
 textoResultado.textContent = historiaFinal;
 caixaAlternativas.textContent = "";
 }
-
 mostraPergunta();
