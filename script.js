99e340b46014aec81495424662d0fb68f7d976db
GuilhermeSilvaCarvalho11 <137510498+GuilhermeSilvaCarvalho11@users.noreply.github.com>
2025-08-26 11:19:46 -0300

    s

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "Em 2059...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
